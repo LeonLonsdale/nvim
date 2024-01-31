6493bfbc1f56b519383e3da68061026fd91cad10
LeonLonsdale <leon.coding@icloud.com>
2024-01-31 23:03:38 +0000

    v1

---
+++ lua/vim-options.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = ' '
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.autoindent = true
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.termguicolors = true
+
+vim.o.mouse = 'a'
