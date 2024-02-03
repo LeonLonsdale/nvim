10eca782dd31daf159390e45eac9692afd13faaa
LeonLonsdale <leon.coding@icloud.com>
2024-02-03 17:30:45 +0000

    Add LazyGit, partially centralise mappings
    Moving keymaps to core.mappings.lua

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,6 +2,5 @@ return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
 		require("gitsigns").setup()
-		vim.keymap.set('n', '<leader>gp', '<cmd>:Gitsigns preview_hunk<cr>')
 	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,27 +1,27 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', 
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-			local builtin = require('telescope.builtin')
+			local builtin = require("telescope.builtin")
 
-			vim.keymap.set('n', '<c-p>', builtin.find_files, {})
-			vim.keymap.set('n', '<Space><Space>', builtin.oldfiles, {})
-			vim.keymap.set('n', '<Space>fg', builtin.live_grep, {})
-			vim.keymap.set('n', '<Space>fh', builtin.help_tags, {})
+			vim.keymap.set("n", "<c-p>", builtin.find_files, {})
+			vim.keymap.set("n", "<Space><Space>", builtin.oldfiles, {})
+			vim.keymap.set("n", "<Space>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<Space>fh", builtin.help_tags, {})
 		end,
 	},
 	{
-		'nvim-telescope/telescope-ui-select.nvim',
+		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
-			require('telescope').setup({
+			require("telescope").setup({
 				extensions = {
-					['ui-select'] = {
-						require('telescope.themes').get_dropdown {}
-					}
-				}
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
 			})
-			require('telescope').load_extension('ui-select')
+			require("telescope").load_extension("ui-select")
 		end,
 	},
 }
